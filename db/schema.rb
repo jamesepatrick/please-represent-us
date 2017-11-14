6fc0290e033bb762e177241b77f979d5a3f8e976
James Patrick <james@jpatrick.io>
2017-11-14 01:31:09 -0500

    Accidental incomplete stash. Reverting for clean migration.

--- db/schema.rb
+++ db/schema.rb
@@ -37,18 +37,6 @@ ActiveRecord::Schema.define(version: 20171114061622) do
     t.index ["parent_id"], name: "index_committees_on_parent_id"
   end
 
-  create_table "contact_infos", force: :cascade do |t|
-    t.string "fax"
-    t.string "phone"
-    t.string "facebook"
-    t.string "twitter"
-    t.string "youtube"
-    t.bigint "legislator_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["legislator_id"], name: "index_contact_infos_on_legislator_id"
-  end
-
   create_table "legislators", force: :cascade do |t|
     t.string "firstname"
     t.string "lastname"
@@ -66,5 +54,4 @@ ActiveRecord::Schema.define(version: 20171114061622) do
 
   add_foreign_key "committee_memberships", "committees"
   add_foreign_key "committee_memberships", "legislators"
-  add_foreign_key "contact_infos", "legislators"
 end
